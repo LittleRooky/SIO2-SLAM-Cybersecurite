e9c6816f24f64f34ef8c1c593b3816a027d0abdf
970M <gd.guillaume.daguet@gmail.com>
2021-10-10 01:08:30 -0400

    + lire traiter fichier, mod. diverses

--- workspace/dev/projet_pyhton/gd_ouvrir_fichier.py
+++ workspace/dev/projet_pyhton/gd_ouvrir_fichier.py
@@ -1,15 +1,35 @@
-
 #!/usr/bin/python3
+#
+# @File : gd_ouvrir_fichier.py
+#
+# @Authors : Guillaume DAGUET
+#            gd.guillaume.daguet@gmail.com
+#            [+|]
+#
+# @Date : 01/10/2021
+#
+# @Version : V1.0
+#
+# @Synopsis : 
+#       
+#       Ouverture d'un fichier en lecture : open(fichier, "r")
+#           => Méthodes read(), readline() et readlines()
+#       Ouverture d'un fichier en écriture : open(fichier, "w") ou open(fichier, "a")
+#           => Méthode write()
+#
+#       r, pour une ouverture en lecture (READ).
+#       w, pour une ouverture en écriture (WRITE), à chaque ouverture le contenu du fichier est écrasé. Si le fichier n'existe pas python le crée.
+#       a, pour une ouverture en mode ajout à la fin du fichier (APPEND). Si le fichier n'existe pas python le crée.
+#
 
 # =============================================================================
-# Importation des librairies
+# IMPORTATION DES LIBRAIRIES
 # =============================================================================
 
-import csv
 import datetime
 
 # =============================================================================
-# Déclaration des variables globales
+# DECLARATION DES VARIABLES GLOBALES
 # =============================================================================
 
 ### Recuperer la date courante
@@ -17,16 +37,19 @@ dateISOFormat = datetime.datetime.now()
 date = dateISOFormat.isoformat(" ", "minutes")
 
 ### Configuration
-repEntree = "/home/gd/Workspace/dev/SIO2-SLAM-Cybersecurite/workspace/database" # Chemin du repertoire d'entree
-repSortie = "/home/gd/Workspace/dev/SIO2-SLAM-Cybersecurite/workspace/tmp" # Chemin du repertoire de sortie
+repEntree = "/home/gyom/GD_W012X/gd_workspace/dev/repositories/SIO2-SLAM-Cybersecurite/workspace/database" # Chemin du repertoire d'entree
+repSortie = "/home/gyom/GD_W012X/gd_workspace/dev/repositories/SIO2-SLAM-Cybersecurite/workspace/tmp" # Chemin du repertoire de sortie
 #nom_fichier = 'agile.txt'
 nom_fichier = 'villes_guadeloupe.csv'
 fichEntree = repEntree + '/' + nom_fichier # Chemin complet vers le fichier d'entree ('+' = concatenation de chaine)
 fichSortie = repSortie + '/' + nom_fichier + '.' + date # Chemin complet vers le fichier de sortie
 
 # =============================================================================
-# Declaration des fonctions
+# DECLARATION DES FONCTIONS
 # =============================================================================
+
+
+# >>> [[ Ouvrir un fichier en lecture ]]
 
 ### Methode 1 avec read : Fichier ==> chaine de caractere
 def ouvir_fichier_read(fichier):
@@ -35,7 +58,7 @@ def ouvir_fichier_read(fichier):
     Méthode classique d'ouverture de fichier.
     """
 
-    filin = open(fichier, 'r')  # Ouverture du fichier, filin est un descripteur de fichier
+    filin = open(fichier, 'r')  # Ouvrir le fichier en lecture, filin est un descripteur de fichier (filin pour les fichier d'entrée)
     strFichier = filin.read()   # Appel la méthode read() de l'objet filin
     filin.close()               # Fermeture du fichier  
  
@@ -53,41 +76,80 @@ def ouvir_fichier_read_bis(fichier):
 
     return strFichier                  # Retourne la variable de type chaine de caratere (string) contenant le
 
-### Methode 2 (pour info) avec readline : Fichier ==> ligne par ligne dans une chaine de caractere
+### Methode 2 avec readline : Fichier ==> Ligne par ligne du ficher dans une chaine de caratere
 def ouvir_fichier_readline(fichier):
     """
-    Ouvir fichier, le parcourir ligne par ligne et stocker le contenu de la ligne courante dans une chaine de caractere
+    Ouvir le fichier, le parcourir ligne par ligne et stocker le contenu de la ligne courante dans une chaine de caractere
     """
-    with open(fichier, "r") as filin:   # Ouvrir le fichier, filin est un descripteur de fichier
-        strLigne = filin.readline()     # La méthode readline() lis la premiere ligne du fichier et fait pointer filin sur la ligne suivante 
-        while strLigne != "":           # Boucler tant que filin n'a pas atteind la fin du fichier (dans ce cas strLigne == "")
-            strLigne = filin.readline() # Recuperer la ligne courante du ficher et on la stock dans strLigne
-    
-    # return strLigne                   # Le return ici n'est pas trés utile car strLigne ne contient que ""
 
-### Methode 2bis avec readline : Fichier ==> chaque ligne du ficher dans une colonne de tableau (liste)
+    with open(fichier, "r") as filin:  # Ouvrir le fichier en lecture, filin est un descripteur de fichier (filin pour les fichier d'entrée)
+        
+        strLigne = filin.readline().rstrip()     # La méthode readline() lis la premiere ligne du fichier et fait pointer filin sur la ligne suivante 
+        
+        # Fonction de TRAITEMENT A FAIRE (initialisation avant le while) :
+        # ex: stocker strLigne dans un tableau comme dans ouvir_fichier_readline_tab
+        #     ou faire un traitement sur strLigne (remplacer un caractere) 
+        #     puis stocker le résultat dans un fichier...
+        # 
+        
+        while strLigne != "":                    # Boucler tant que filin n'a pas atteind la fin du fichier (dans ce cas strLigne == "")
+            strLigne = filin.readline().rstrip() # Recuperer la ligne courante du ficher et on la stack dans strLigne
+            
+            # Fonction de TRAITEMENT A FAIRE à chaque itération :
+            # ex: stocker strLigne dans un tableau comme dans ouvir_fichier_readline_tab
+            #     ou faire un traitement sur strLigne (remplacer un caractere) 
+            #     puis stocker le résultat dans un fichier...
+            # 
+
+    # return foo # Suivant le traitement on mettra ou non un return 
+
+### Ex. de Methode 2 avec readline : Fichier ==> chaque ligne du ficher dans une colonne de tableau (liste)
 def ouvir_fichier_readline_tab(fichier):
     """
     Ouvir fichier, le parcourir ligne par ligne et stocker ajouter la ligne courante a la fin tableau
     """
-    lstFichier = []                         # Déclaration d'un tableau vide
-    with open(fichier, "r") as filin:       # Ouvrir du fichier, filin est un descripteur de fichier      
-        strLigne = filin.readline().rstrip()          # La méthode readline() lis la premiere ligne du fichier et fait pointer filin sur la ligne suivante 
-        lstFichier.append(strLigne)         # Ajouter avec append la ligne courante a la fin du tableau
-        while strLigne != "":               # Boucler tant que filin n'a pas atteind la fin du fichier (dans ce cas strLigne == "")
-            strLigne = filin.readline().rstrip()     # Recuperer la ligne courante du ficher et on la stack dans strLigne
-            lstFichier.append(strLigne)     # Ajouter avec append la ligne courante a la fin du tableau
-    return lstFichier                       # Retourner lstFichier, un tableau de chaine de caractere contenant toute les lignes du fichier
+
+    with open(fichier, "r") as filin:            # Ouvrir le fichier en lecture, filin est un descripteur de fichier (filin pour les fichier d'entrée)
+
+        strLigne = filin.readline().rstrip()     # La méthode readline() lis la premiere ligne du fichier et fait pointer filin sur la ligne suivante 
+        
+        # TRAITEMENT A FAIRE (initialisation avant le while)
+        lstFichier = []                          # Déclaration d'un tableau vide
+        lstFichier.append(strLigne)              # Ajouter avec append la ligne courante a la fin du tableau
+        # Fin traitement
+
+        while strLigne != "":                    # Boucler tant que filin n'a pas atteind la fin du fichier (dans ce cas strLigne == "")
+
+            # TRAITEMENT A FAIRE (à chaque itération)
+            strLigne = filin.readline().rstrip() # Recuperer la ligne courante du ficher et on la stack dans strLigne
+            lstFichier.append(strLigne)          # Ajouter avec append la ligne courante a la fin du tableau
+            # Fin traitement
+
+    return lstFichier                            # Retourner lstFichier, un tableau de chaine de caractere contenant toute les lignes du fichier
+
 
 ### Methode 3 avec readlines : Fichier ==> chaque ligne du ficher dans une colonne de tableau (liste)
 def ouvir_fichier_readlines(fichier):
     """
     Ouvrir le fichier et placer directement chaque ligne du fichier dans une case de tableau
     """
-    with open(fichEntree, 'r') as filin:
-        strFichier = filin.readlines()  # La methode readlines placer directement chaque ligne du fichier dans une case de tableau
+
+    with open(fichEntree, 'r') as filin: # Ouvrir le fichier en lecture, filin est un descripteur de fichier (filin pour les fichier d'entrée)
+        strFichier = filin.readlines()   # La methode readlines placer directement chaque ligne du fichier dans une case de tableau
 
     return strFichier
+
+# >>> [[ Ouvrir un fichier en écriture ]]
+
+### Ouvrir un fichier en écriture (WRITE)
+def ouvir_fichier_write(fichier): # Ouvrir le fichier en écriture, filout est un descripteur de fichier (filout pour les fichier de sortie)
+    """
+    Ouvir le fichier en écriture et ajouter du texte dedans
+    """
+
+    with open(fichier, "w") as filout:  # On pourra mettre également "a"
+        filout.write("Hello World!")    # Appel la méthode write() de l'objet filout
+
 
 ##############################################################################
 # MAIN : Partie principal du programme
@@ -116,3 +178,8 @@ print(fichier_liste2)
 print("type de la variable :", type(fichier_liste2))
 
 #print(ouvir_fichier_read_bis.doc)
+
+### WRITE
+print("\nMethode 4 avec write :\n")
+ouvir_fichier_write(fichSortie)
+print("ovrez : ", fichSortie )
---
+++ workspace/dev/projet_pyhton/lire_traiter_ecrire_fichier.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+#
+# @File : lire_traiter_ecrire_fichier.py
+#
+# @Authors : Guillaume DAGUET
+#            gd.guillaume.daguet@gmail.com
+#            [+|]
+#
+# @Date : 01/10/2021
+#
+# @Version : V1.0
+#
+# @Synopsis : 
+#       
+#       Ouverture d'un fichier en lecture : open(fichier, "r")
+#           => Méthodes readline()
+#       Méthode de modification des listes : replace(), lower()
+#       Ouverture d'un fichier en écriture : open(fichier, "w") ou open(fichier, "a")
+#           => Méthode write()
+#
+
+# =============================================================================
+# IMPORTATION DES LIBRAIRIES
+# =============================================================================
+
+import datetime
+
+# =============================================================================
+# DECLARATION DES VARIABLES GLOBALES
+# =============================================================================
+
+### Recuperer la date courante
+dateISOFormat = datetime.datetime.now()
+date = dateISOFormat.isoformat(" ", "minutes")
+
+### Configuration
+repEntree = "/home/gyom/GD_W012X/gd_workspace/dev/repositories/SIO2-SLAM-Cybersecurite/workspace/database" # Chemin du repertoire d'entree
+repSortie = "/home/gyom/GD_W012X/gd_workspace/dev/repositories/SIO2-SLAM-Cybersecurite/workspace/tmp" # Chemin du repertoire de sortie
+#nom_fichier = 'agile.txt'
+nom_fichier = 'villes_guadeloupe.csv'
+fichEntree = repEntree + '/' + nom_fichier # Chemin complet vers le fichier d'entree ('+' = concatenation de chaine)
+fichSortie = repSortie + '/' + nom_fichier + '.' + date # Chemin complet vers le fichier de sortie
+
+
+# =============================================================================
+# DECLARATION DES FONCTIONS
+# =============================================================================
+
+def lire_traiter_ecrire_fichier(fEntree, fSortie):
+    """
+    Ouvir le fichier fichEntree, le parcourir ligne par ligne, effectuer un traitement sur le contenu de 
+    la ligne courante puis écrire le résultat dans le fichier de sortie fichSortie
+    """
+
+    with open(fSortie, "w") as filout: # Ouvir le fichier de sortie en écriture
+        
+        with open(fEntree, "r") as filin:  # Ouvrir le fichier d'entrée en lecture
+            
+            strLigne = filin.readline()  # La méthode readline() lis la premiere ligne du fichier et fait pointer filin sur la ligne suivante 
+            
+            # TRAITEMENT A FAIRE (initialisation avant le while) :
+            strNewLigne  = strLigne.replace('a', 'z').replace('e', 'o').lower() # Remplacer des carateres et mettre en minuscule
+            filout.write(strNewLigne) # Ecrire la ligne traitée dans le fichier de sortie
+
+            while strLigne != "":                    # Boucler tant que filin n'a pas atteind la fin du fichier (dans ce cas strLigne == "")
+       
+                strLigne = filin.readline() # Recuperer la ligne courante du ficher et on la stack dans strLigne
+                    
+                # TRAITEMENT A FAIRE à chaque itération :
+                strNewLigne  = strLigne.replace('a', 'z').replace('e', 'o').lower() # Remplacer des carateres et mettre en minuscule
+                filout.write(strNewLigne) # Ecrire la ligne traitée dans le fichier de sortie
+    
+    # return foo # Suivant le traitement on mettra ou non un return
+
+##############################################################################
+# MAIN : Partie principal du programme
+##############################################################################
+
+lire_traiter_ecrire_fichier(fichEntree, fichSortie)
+print("\nfichier généré : \n", fichSortie)
+
